1af08487bf30bb47d201d6d937d300ff7e161066
m1ke <git@m1ke.me>
2019-02-09 16:40:07 +0000

    Remove db params to attempt db creation working

--- database.tf
+++ database.tf
@@ -11,13 +11,11 @@ resource "aws_db_instance" "example" {
   security_group_names = []
   vpc_security_group_ids = [
     "${aws_security_group.db.id}"]
-  parameter_group_name = "${aws_db_parameter_group.example.name}"
   multi_az = false
   backup_retention_period = 0
   backup_window = "01:00-02:00"
   maintenance_window = "mon:06:00-mon:06:30"
   final_snapshot_identifier = "example-db-final"
-  iam_database_authentication_enabled = true
   monitoring_interval = 0
   skip_final_snapshot = false
   apply_immediately = true
@@ -29,15 +27,4 @@ resource "aws_db_instance" "example" {
 
 output "db-endpoint" {
   value = "${aws_db_instance.example.endpoint}"
-}
-
-resource "aws_db_parameter_group" "basic" {
-  name   = "basic"
-  family = "mysql5.6"
-  description = "Minor tweaks to regular mysql config"
-
-  parameter {
-    name  = "time_zone"
-    value = "europe/dublin"
-  }
 }
